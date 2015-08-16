d9b3cf5cd4919981fcad8a3e6322cd53ab0882c7
waqt <tjliu730@126.com>
2015-08-17 01:34:46 +0800

    init

---
+++ scripts/ownerone.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+npm owner add joshuatrii $1
+npm owner add benirose $1
+npm owner add kylestetz $1
+npm owner add boutell $1
+npm owner add colpanik $1
+npm owner add jsumnersmith $1
+npm owner add alexgilbert $1
+npm owner add stuartromanek $1
+npm owner add livhaas $1
+npm owner add mcoppola $1
+npm owner add suhmantha1 $1
+npm owner add jimmyh $1
